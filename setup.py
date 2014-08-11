18f76f9a8944ee3a333b11dafa3a2362a43162ca
andrey <bloogrox@gmail.com>
2014-08-11 20:13:06 +0400

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='http_build_query',
+    version='0.1.0',
+    author='Aslan',
+    author_email='bloogrox@gmail.com',
+    py_modules=['http_build_query'],
+    url='https://github.com/bloogrox/http_build_query.git',
+    description='Works same as php http_build_query function.',
+)
